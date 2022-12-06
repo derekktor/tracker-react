cea83026b660a22f2e9e2fe35058e36cf9bb4c20
Derek <baldka1109bh@gmail.com>
2022-12-06 16:49:58 +0100

    Go back and forth in navigation history

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="footer">
-        <h1>Footer</h1>
+        <button onClick={() => navigate(-1)}>Back</button>
+        <button onClick={() => navigate(1)}>Forward</button>
     </div>
   )
 }
