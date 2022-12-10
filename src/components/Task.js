19e8c6360fe33ae632101193cf674b19a275182a
Derek <baldka1109bh@gmail.com>
2022-12-10 14:29:47 +0100

    Prettify task component

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,19 +3,22 @@ import { FaTimes } from "react-icons/fa";
 
 const Task = ({ task, onDelete, onToggle }) => {
     return (
-        <div 
+        <div
             onDoubleClick={() => onToggle(task.id)}
-            className={task.isActive ? "task active" : "task"}>
+            className={task.isActive ? "task active" : "task"}
+        >
             <div className="flex-horizontal">
-                <h4>{task.title}</h4>
-                <FaTimes
-                    className="cross"
-                    onClick={() => onDelete(task.id)}
-                />
+                <div className="ctx">
+                    <h4>{task.title}</h4>
+                    <h5>{task.desc}</h5>
+                </div>
+                <FaTimes className="cross" onClick={() => onDelete(task.id)} />
             </div>
-            <p><i>{task.desc}</i></p>
+            <div className="flex-horizontal">
             <p>{task.createdAt}</p>
             <p>{task.finishedAt}</p>
+
+            </div>
         </div>
     );
 };
